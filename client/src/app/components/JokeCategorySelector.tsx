a7246a11814192957bde41af2a197c5a917c9515
Matheus Dias <matheus.dias@minhavisita.com.br>
2024-06-21 15:36:23 -0300

    feat: Set default selected category in JokeCategorySelector component and styling

--- client/src/app/components/JokeCategorySelector.tsx
+++ client/src/app/components/JokeCategorySelector.tsx
@@ -9,7 +9,11 @@ const GET_JOKE_CATEGORIES = gql`
 `;
 
 function JokeCategorySelector() {
-  const { loading, error, data } = useQuery(GET_JOKE_CATEGORIES);
+  const { loading, error, data } = useQuery(GET_JOKE_CATEGORIES, {
+    onCompleted: (data) => {
+      setJokeInfos({ ...jokeInfos, selectedCategory: data.categories[0] });
+    }
+  });
   const { jokeInfos, setJokeInfos } = useJokeInfosContext();
 
   const handleCategoryChange = (event: any) => {
@@ -17,14 +21,17 @@ function JokeCategorySelector() {
     setJokeInfos({ ...jokeInfos, selectedCategory });
   };
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) return;
 
   return (
-    <select onChange={handleCategoryChange} value={jokeInfos.selectedCategory}>
+    <select
+      onChange={handleCategoryChange}
+      value={jokeInfos.selectedCategory}
+      className="p-4 border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md shadow-sm"
+    >
       {data.categories.map((category: string) => (
         <option key={category} value={category}>
-          {category}
+          {category.charAt(0).toUpperCase() + category.slice(1)}
         </option>
       ))}
     </select>
