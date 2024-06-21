0cc532984b6b5f0d1a26d579c697f005ae492ea7
Matheus Dias <matheus.dias@minhavisita.com.br>
2024-06-21 15:36:49 -0300

    chore: updating Home component layout and structure

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -11,18 +11,15 @@ const client = createApolloClient();
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+    <main className="bg-gray-100 min-h-screen flex flex-col gap-4 p-4 items-center justify-center">
       <JokeInfosContextProvider>
         <ApolloProvider client={client}>
           <header className="text-4xl font-bold text-center text-gray-800 my-8">
             <h1>Chuck Norris Facts</h1>
           </header>
-          
-          <JokeCategorySelector/>
 
-          <section className="p-4 bg-white rounded-lg shadow-md">
-            <JokeDisplay />
-          </section>
+          <JokeCategorySelector/>
+          <JokeDisplay />
         </ApolloProvider>
       </JokeInfosContextProvider>
     </main>
