844005b590fc75c4509cdacae1f420544b97af7a
Matheus Dias <matheus.dias@minhavisita.com.br>
2024-06-21 15:36:41 -0300

    feat: adding event listener for fetching new joke

--- client/src/app/components/JokeDisplay.tsx
+++ client/src/app/components/JokeDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { gql, useQuery } from '@apollo/client';
 import { useJokeInfosContext } from '../context/JokeInfosContext';
 
@@ -12,10 +12,10 @@ const GET_JOKE_QUERY = gql`
 `;
 
 const JokeDisplay = () => {
-  const { jokeInfos, setJokeInfos } = useJokeInfosContext();
+  const { jokeInfos } = useJokeInfosContext();
   const [joke, setJoke] = useState('');
 
-  const { loading, error, refetch } = useQuery(GET_JOKE_QUERY, {
+  const { error, refetch } = useQuery(GET_JOKE_QUERY, {
     variables: { category: jokeInfos.selectedCategory },
     onCompleted: (data) => {
       setJoke(data.randomJoke.value);
@@ -23,18 +23,37 @@ const JokeDisplay = () => {
     skip: !jokeInfos.selectedCategory,
   });
 
+  useEffect(() => {
+    const handleKeyPress = (event: any) => {
+      if (event.key === 'Enter') {
+        fetchNewJoke();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [jokeInfos.selectedCategory]); // Re-bind the event listener if the selected category changes
+
   const fetchNewJoke = async () => {
     const { data } = await refetch();
     setJoke(data.randomJoke.value);
   };
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error) return <p className="text-red-500">Error :(</p>;
 
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md">
-      <p>{joke}</p>
-      <button onClick={fetchNewJoke}>Fetch New Joke</button>
+    <div className="flex flex-col items-center justify-center p-4 w-full bg-white rounded-lg shadow-md">
+      <p className="text-lg text-gray-800 mb-4 text-center">{joke}</p>
+      <button 
+        onClick={fetchNewJoke} 
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors duration-200 ease-in-out mb-2"
+      >
+        Fetch New Joke
+      </button>
+      <p className="text-xs text-gray-500">Press Enter to fetch a new joke</p>
     </div>
   );
 };
